cc0b888b3a544d04f6e9d867e38d13618b59edc9
Hailey Bongo <haileyebongo@gmail.com>
2020-04-21 20:17:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,5 @@ require './config/environment'
 
 class App < Sinatra::Base
    
-   get '/' do 
-    erb :super_hero
-  end
-  
-  post '/teams' do
-    heroes = params[]
-    binding.pry
-    erb :team
-  end
-  
+
 end
